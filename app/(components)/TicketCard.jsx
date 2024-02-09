1f969423f4f0ca0cedff73b2742c62aff751d715
Himanshi Sharma <himanshish456@gmail.com>
2024-02-09 22:19:39 +0530

    layout

---
+++ app/(components)/TicketCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import DeleteBlock from "./DeleteBlock";
+import PriorityDisplay from "./PriorityDisplay";
+import ProgressDisplay from "./ProgressDisplay";
+
+const TicketCard = () => {
+  return (
+    <div>
+      <DeleteBlock />
+      <PriorityDisplay />
+      <ProgressDisplay />
+    </div>
+  );
+};
+
+export default TicketCard;
