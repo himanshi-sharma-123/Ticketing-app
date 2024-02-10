6e38f01e9268c13a8bc91d0e37188fff9dcc4bed
Himanshi Sharma <himanshish456@gmail.com>
2024-02-10 20:53:45 +0530

    database to screen

--- app/page.jsx
+++ app/page.jsx
@@ -26,7 +26,11 @@ const Dashboard = async () => {
               {tickets
                 .filter((ticket) => ticket.category === uniqueCategory)
                 .map((filteredTicket, _index) => (
-                  <TicketCard id={_index} key={_index} />
+                  <TicketCard
+                    id={_index}
+                    key={_index}
+                    ticket={filteredTicket}
+                  />
                 ))}
             </div>
           ))}
